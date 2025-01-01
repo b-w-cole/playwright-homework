044b5c0da051bdbf10e50bb31b28611a043d86a7
Brandon Cole <bcole128@outlook.com>
2025-01-01 12:38:29 -0800

    Adding support for visual testing for New Owner Page

---
+++ tests/visualTesting.spec.ts
@@ -0,0 +1,30 @@
+import { test } from './test-options'
+import { RandomDataHelper } from '../pages/randomDataHelper'
+import { expect } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+})
+
+test('visual testing', async ({ pageManager }) => {
+
+    const firstName = 'FirstName'
+    const lastName = 'LastName'
+    const address = 'Address'
+    const city = 'City'
+    const telephone = 'Telephone'
+
+    await pageManager.navigateTo().ownersPage()
+    await pageManager.onOwnersPage().clickAddOwner()
+
+    const newOwnerFormLocator = await pageManager.onNewOwnerPage().getNewOwnerFormLocatorForVisualTesting()
+
+    await expect(newOwnerFormLocator).toHaveScreenshot('newFormScreenshot-1.png')
+
+    await pageManager.onNewOwnerPage().fillNewOwnerForm(firstName, lastName, address, city, telephone)
+
+    const newOwnerFormLocator2 = await pageManager.onNewOwnerPage().getNewOwnerFormLocatorForVisualTesting()
+
+    await expect(newOwnerFormLocator2).toHaveScreenshot('newFormScreenshot-2.png')
+
+})
